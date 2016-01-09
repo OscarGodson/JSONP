9863d186b251917856cd99187374065c699faa18
16patsle <patricksletvold@hotmail.com>
2016-01-09 19:34:48 +0100

    Rewrite to use Promises
    Added Promises as an option instead of callbacks. Not natively
    compatible with IE.

--- JSONP.js
+++ JSONP.js
@@ -3,7 +3,7 @@
  * @param {String} url  The URL you are requesting with the JSON data
  * @param {Object} data The Data object you want to generate the URL params from
  * @param {String} method  The method name for the callback function. Defaults to callback (for example, flickr's is "jsoncallback")
- * @param {Function} callback  The callback you want to execute as an anonymous function. The first parameter of the anonymous callback function is the JSON
+ * @param {Function} callback  The callback you want to execute as an anonymous function. Use this if you do not want to use Promises.
  *
  * @example
  * JSONP('http://twitter.com/users/oscargodson.json',function(json){
@@ -11,7 +11,7 @@
  * });
  *
  * @example
- * JSONP('http://api.flickr.com/services/feeds/photos_public.gne',{'id':'12389944@N03','format':'json'},'jsoncallback',function(json){
+ * JSONP('http://api.flickr.com/services/feeds/photos_public.gne',{'id':'12389944@N03','format':'json'},'jsoncallback').then(function(json){
  *  document.getElementById('flickrPic').innerHTML = '<p>Flickr Pic:</p><img src="'+json.items[0].media.m+'">';
  * });
  *
@@ -20,90 +20,106 @@
  *  document.getElementById('facebook').innerHTML = json.about;
  * });
  */
-(function( window, undefined) {
-  var JSONP = function(url,data,method,callback){
-    //Set the defaults
-    url = url || '';
-    data = data || {};
-    method = method || '';
-    callback = callback || function(){};
-    
-    //Gets all the keys that belong
-    //to an object
-    var getKeys = function(obj){
-      var keys = [];
-      for(var key in obj){
-        if (obj.hasOwnProperty(key)) {
-          keys.push(key);
-        }
-        
-      }
-      return keys;
-    }
+(function(window, undefined) {
+    var JSONP = function(url, data, method, callback) {
+        return new Promise(function(resolve, reject) {
+            //Set the defaults
+            url = url || '';
+            data = data || {};
+            method = method || '';
+            callback = callback || function() {};
 
-    //Turn the data object into a query string.
-    //Add check to see if the second parameter is indeed
-    //a data object. If not, keep the default behaviour
-    if(typeof data == 'object'){
-      var queryString = '';
-      var keys = getKeys(data);
-      for(var i = 0; i < keys.length; i++){
-        queryString += encodeURIComponent(keys[i]) + '=' + encodeURIComponent(data[keys[i]])
-        if(i != keys.length - 1){ 
-          queryString += '&';
-        }
-      }
-      url += '?' + queryString;
-    } else if(typeof data == 'function'){
-      method = data;
-      callback = method;
-    }
+            //Gets all the keys that belong
+            //to an object
+            var getKeys = function(obj) {
+                var keys = [];
+                for (var key in obj) {
+                    if (obj.hasOwnProperty(key)) {
+                        keys.push(key);
+                    }
 
-    //If no method was set and they used the callback param in place of
-    //the method param instead, we say method is callback and set a
-    //default method of "callback"
-    if(typeof method == 'function'){
-      callback = method;
-      method = 'callback';
-    }
-  
-    //Check to see if we have Date.now available, if not shim it for older browsers
-    if(!Date.now){
-      Date.now = function() { return new Date().getTime(); };
-    }
+                }
+                return keys;
+            };
 
-    //Use timestamp + a random factor to account for a lot of requests in a short time
-    //e.g. jsonp1394571775161 
-    var timestamp = Date.now();
-    var generatedFunction = 'jsonp'+Math.round(timestamp+Math.random()*1000001)
+            //Turn the data object into a query string.
+            //Add check to see if the second parameter is indeed
+            //a data object. If not, keep the default behaviour
+            if (typeof data == 'object') {
+                var queryString = '';
+                var keys = getKeys(data);
+                for (var i = 0; i < keys.length; i++) {
+                    queryString += encodeURIComponent(keys[i]) + '=' +
+                        encodeURIComponent(data[
+                            keys[i]]);
+                    if (i != keys.length - 1) {
+                        queryString += '&';
+                    }
+                }
+                url += '?' + queryString;
+            } else if (typeof data == 'function') {
+                method = data;
+                callback = method;
+            }
 
-    //Generate the temp JSONP function using the name above
-    //First, call the function the user defined in the callback param [callback(json)]
-    //Then delete the generated function from the window [delete window[generatedFunction]]
-    window[generatedFunction] = function(json){
-      callback(json);
+            //If no method was set and they used the callback param in place of
+            //the method param instead, we say method is callback and set a
+            //default method of "callback"
+            if (typeof method == 'function') {
+                callback = method;
+                method = 'callback';
+            }
 
-      // IE8 throws an exception when you try to delete a property on window
-      // http://stackoverflow.com/a/1824228/751089
-      try {
-        delete window[generatedFunction];
-      } catch(e) {
-        window[generatedFunction] = undefined;
-      }
+            if (method === "") {
+                method = "callback";
+            }
 
+            //Check to see if we have Date.now available, if not shim it for older browsers
+            if (!Date.now) {
+                Date.now = function() {
+                    return new Date().getTime();
+                };
+            }
+
+            //Use timestamp + a random factor to account for a lot of requests in a short time
+            //e.g. jsonp1394571775161
+            var timestamp = Date.now();
+            var generatedFunction = 'jsonp' + Math.round(timestamp + Math.random() *
+                1000001);
+
+            //Generate the temp JSONP function using the name above
+            //First, call the function the user defined in the callback param [callback(json)]
+            //Then delete the generated function from the window [delete window[generatedFunction]]
+            window[generatedFunction] = function(json) {
+                callback(json);
+
+                resolve(json);
+
+                // IE8 throws an exception when you try to delete a property on window
+                // http://stackoverflow.com/a/1824228/751089
+                try {
+                    delete window[generatedFunction];
+                } catch (e) {
+                    window[generatedFunction] = undefined;
+                }
+
+            };
+
+            //Check if the user set their own params, and if not add a ? to start a list of params
+            //If in fact they did we add a & to add onto the params
+            //example1: url = http://url.com THEN http://url.com?callback=X
+            //example2: url = http://url.com?example=param THEN http://url.com?example=param&callback=X
+            if (url.indexOf('?') === -1) {
+                url = url + '?';
+            } else {
+                url = url + '&';
+            }
+
+            //This generates the <script> tag
+            var jsonpScript = document.createElement('script');
+            jsonpScript.setAttribute("src", url + method + '=' + generatedFunction);
+            document.getElementsByTagName("head")[0].appendChild(jsonpScript);
+        });
     };
-
-    //Check if the user set their own params, and if not add a ? to start a list of params
-    //If in fact they did we add a & to add onto the params
-    //example1: url = http://url.com THEN http://url.com?callback=X
-    //example2: url = http://url.com?example=param THEN http://url.com?example=param&callback=X
-    if(url.indexOf('?') === -1){ url = url+'?'; }
-    else{ url = url+'&'; }
-  
-    //This generates the <script> tag
-    var jsonpScript = document.createElement('script');
-    jsonpScript.setAttribute("src", url+method+'='+generatedFunction);
-    document.getElementsByTagName("head")[0].appendChild(jsonpScript)
-  }
-  window.JSONP = JSONP;
+    window.JSONP = JSONP;
 })(window);
--- README.md
+++ README.md
@@ -1,43 +1,51 @@
-#JSONP Beta 1.0
-##Alpha Stage
+# JSONP Beta 1.0
+## Alpha Stage
 
-###What 
-This is a super tiny JSONP library for when you don't need jQuery or another JS library just for JSONP requests.
+### What
+This is a super tiny JSONP library for when you don't need jQuery or another JS library just for JSONP requests. It supports both callbacks and [Promises](http://www.html5rocks.com/en/tutorials/es6/promises/) for easy use asynchronous use.
 
-###Why
+### Why
 After searching for "JSONP examples" and "JSONP tutorials" I was dissapointed by the lack of straightforward JSONP stuff out there. While there is nothing wrong with jQuery and I use it every single day, sometimes you need to do some JSONP without using jQuery and so I wrote this for myself, but feel free to use it! The min version is only 621 bytes.
 
-###How
+### How
 
-It's simple. There are three parameters, but you only need two. This is what it looks like:
+It's simple. There are three parameters, but you only need one. This is what it looks like:
 
-    JSONP([url],[data], [method],[callback])
+    JSONP([url],[data], [method])
 
 To get my Twitter avatar for example you'd do:
-
-    JSONP('http://twitter.com/users/oscargodson.json',function(json){
-      console.log(json.profile_image_url);
+    ```javascript
+    JSONP('http://twitter.com/users/oscargodson.json').then(function(json){
+        console.log(json.profile_image_url);
     });
-
+    ```
 Sometimes the third-party site require a custom method name, like Flickr, which requires `jsoncallback` rather than the standard `callback` method. Here's an example to get a thumbnail of my latest pic on Flickr:
-
-    JSONP('http://api.flickr.com/services/feeds/photos_public.gne?id=12389944@N03&format=json','jsoncallback',function(json){
+    ```javascript
+    JSONP('http://api.flickr.com/services/feeds/photos_public.gne?id=12389944@N03&format=json','jsoncallback').then(function(json){
       console.log(json.items[0].media.m);
     });
-
+    ```
 You can also pass parameters in the form of a JSON object, like so:
-
-    JSONP('https://api.github.com/users/carvefx/gists',{'since': '2014-02-01T12:00:00Z'},function(json){
+    ```javascript
+    JSONP('https://api.github.com/users/carvefx/gists',{'since': '2014-02-01T12:00:00Z'}).then(function(json){
       document.getElementById('github').innerHTML = json.data[0].html_url;
     });
+    ```
+Additionally you can use a callback instead, providing one as tghe last parameter:
+    ```javascript
+    JSONP('http://twitter.com/users/oscargodson.json',function(json){
+        console.log(json.profile_image_url);
+    });
+    ```
 
-###Browser Support
-* IE6+
-* Firefox 3.0+
-* Safari 4.0+
-* Google Chrome 14.0+
-* Opera 10.6+
+### Browser Support
+* IE6+ (using a polyfill like [Babel](babeljs.io) or [ES6-Promise](https://github.com/jakearchibald/es6-promise) )
+* Edge 13+
+* Firefox 29.0+
+* Safari 7.1+
+* Google Chrome 32.0+
+* Opera 19+
 
-###To do...
+### To do...
 
 * Anything else people request, if anything
